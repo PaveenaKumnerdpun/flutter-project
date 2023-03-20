5707ba1a4b340547f7da89a8059f82830dd1c9ea
Supakitt Surojanakul <81941466+SaturnFoxOhm@users.noreply.github.com>
2023-03-20 16:03:22 +0700

    Update SearchPage.dart

--- lib/SearchPage.dart
+++ lib/SearchPage.dart
@@ -49,19 +49,37 @@ class _SearchPageState extends State<SearchPage> {
             ),
 
             const SizedBox(height: 25),
-            Text("City: ${_weather?.data?.city?.name ?? "NULL"}"),
-            Text("Date: ${_weather?.data?.time?.tz ?? "NULL"}"),
-            Text("City Geo: ${_weather?.data?.city?.geo ?? "NULL"}"),
             Text(
-                "Individual AQI for the PM2.5: ${_weather?.data?.iaqi?.pm25?.v ?? "NULL"}"),
+              "City: ${_weather?.data?.city?.name ?? "NULL"}",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 18, height: 2.0),
+            ),
+            Text("Date: ${_weather?.data?.time?.tz ?? "NULL"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 18, height: 2.0)),
+            Text("City Geo: ${_weather?.data?.city?.geo ?? "NULL"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 18, height: 2.0)),
             Text(
-                "Average PM2.5 Forcast: ${_weather?.data?.forecast?.daily?.pm25?[0].avg ?? "NULL"}"),
+                "Individual AQI for the PM2.5: ${_weather?.data?.iaqi?.pm25?.v ?? "NULL"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 18, height: 2.0)),
             Text(
-                "Average PM2.5 Forcast: ${_weather?.data?.forecast?.daily?.pm25?[1].avg ?? "NULL"}"),
+                "Average PM2.5 Forcast: ${_weather?.data?.forecast?.daily?.pm25?[0].avg ?? "NULL"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 18, height: 2.0)),
             Text(
-                "Average PM10 Forcast: ${_weather?.data?.forecast?.daily?.pm10?[0].avg ?? "NULL"}"),
+                "Average PM2.5 Forcast: ${_weather?.data?.forecast?.daily?.pm25?[1].avg ?? "NULL"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 18, height: 2.0)),
             Text(
-                "Average PM10 Forcast: ${_weather?.data?.forecast?.daily?.pm10?[1].avg ?? "NULL"}"),
+                "Average PM10 Forcast: ${_weather?.data?.forecast?.daily?.pm10?[0].avg ?? "NULL"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 18, height: 2.0)),
+            Text(
+                "Average PM10 Forcast: ${_weather?.data?.forecast?.daily?.pm10?[1].avg ?? "NULL"}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 18, height: 2.0)),
           ]),
         ));
   }
